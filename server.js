a96989c5155f568fe672563714ad58b685442fb6
beatface <emma.beaton@gmail.com>
2016-02-18 15:57:51 -0600

    added logger middleware function that logs every request to the database

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const sassMiddleware = require('node-sass-middleware');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 // my modules
+const logger = require('./services/logger');
 const routes = require('./routes/index');
 
 const app = express();
@@ -14,20 +15,20 @@ const PORT = process.env.PORT || 3000;
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(methodOverride('_method'));
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({
     extended: false
 }));
-
-app.use(routes);
-
 // compile sass
 app.use(sassMiddleware({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: true,
-  sourceMap: true
+    src: path.join(__dirname, 'public'),
+    dest: path.join(__dirname, 'public'),
+    indentedSyntax: true,
+    sourceMap: true
 }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger);
+app.use(routes);
+
 
 mongoose.connect('mongodb://localhost:27017/mean-notes');
 const database = mongoose.connection;
---
+++ services/logger.js
@@ -0,0 +1,11 @@
+"use strict";
+const Log = require('./models/logModel');
+
+module.exports = (req, res, next) => {
+    // log request to database
+    Log.create({
+        userAgent: req.headers['user-agent'],
+        route: req.url,
+        verb: req.method
+    }, next);
+}
